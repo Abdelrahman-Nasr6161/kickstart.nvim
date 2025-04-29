f882677d319f08bb4a5deb57ecbf0d155ee4cd26
Abdelrahman-Nasr6161 <abdoahlawy6161@gmail.com>
2025-04-29 15:41:25 +0300

    auto closing parentheses and other pairs

--- init.lua
+++ init.lua
@@ -262,6 +262,11 @@ require('lazy').setup({
       })
     end
   },
+  {
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = true
+},
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
